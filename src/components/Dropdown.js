b22dc34f792e2e85d92ee79e259e5a23d3225999
Ricardo <limaricardop@gmail.com>
2022-04-13 23:27:52 -0300

    Add event listener and capture

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const Dropdown =  ({ options, selected, onSelectedChange }) => {
 
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener('click', () => {
+            setOpen(false);
+        }, { capture: true })
+    }, [])
+
     const renderedOptions = options.map((option) => {
+        if (option.value === selected.value) {
+            return null;
+        }
+
+
         return (
             <div 
                 key={option.value} 
@@ -21,10 +35,10 @@ const Dropdown =  ({ options, selected, onSelectedChange }) => {
                 <label className="label">
                     Select a Color
                 </label>
-                <div className="ui selection dropdown visible active">
+                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
 
